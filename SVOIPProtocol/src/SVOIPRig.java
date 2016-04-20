546b80b0b5e3d4b322d9cba21dadd2784e9600a4
lrolleman <lylerolleman@gmail.com>
2016-04-20 14:15:01 -0600

    clean up

--- SVOIPProtocol/src/SVOIPRig.java
+++ SVOIPProtocol/src/SVOIPRig.java
@@ -13,27 +13,11 @@ public class SVOIPRig {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		NetworkManager.initNetwork();
-		NetworkManager.establishConnection("test", "192.168.0.20", 25123);
-		NetworkManager.sendMessage("test", "CONNECT test;\r\n");
-		/*try {
-			SVOIPConnection con = new SVOIPConnection("192.168.0.20", 25123);
-			con.sendMessage("CONNECT test;\r\n");
-			//con.close();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}*/
-		/*try {
-			Socket sock = new Socket("192.168.0.20", 25123);
-			BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(sock.getOutputStream( )));
-			writer.write("CONNECT test;");
-			writer.flush();
-			writer.close();
-			sock.close();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}*/
+		if (args.length > 0 && args[0].equals("test")) {
+			NetworkManager.establishConnection("test", "192.168.0.20", 25123);
+			NetworkManager.sendMessage("test", "CONNECT test;\r\n");
+		}
+		
 	}
 
 }
